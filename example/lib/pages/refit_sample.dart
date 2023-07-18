1011f6ee096d220e956255698fddc23775c81509
Miguel Beignon <mbgspcii@gmail.com>
2023-07-18 14:09:10 +0200

    Updates refit example.

--- example/lib/pages/refit_sample.dart
+++ example/lib/pages/refit_sample.dart
@@ -7,12 +7,30 @@ class RefitSample extends StatefulWidget {
     State<RefitSample> createState() => _RefitSampleState();
 }
 
+class Config {
+    TwicMode mode;
+    TwicPosition anchor;
+    dynamic? refit;
+    Config( { this.mode = TwicMode.cover, this.anchor = TwicPosition.center, this.refit });
+}
+
 class _RefitSampleState extends State<RefitSample> {
-    final ratios = [ '1', '4/3', '16/9', '21/9' ];
+    static List< Config > config = [
+        Config( mode: TwicMode.cover),
+        Config( mode: TwicMode.cover, refit: true),
+        Config( mode: TwicMode.cover, refit: '10p'),
+        Config( anchor: TwicPosition.left, mode: TwicMode.cover, refit: '10p'),
+        Config( anchor: TwicPosition.right, mode: TwicMode.cover, refit: '10p'),
+        Config( mode: TwicMode.contain),
+        Config( mode: TwicMode.contain, refit: true),
+        Config( mode: TwicMode.contain, refit: '10p'),
+        Config( anchor: TwicPosition.left, mode: TwicMode.contain, refit: '10p'),
+        Config( anchor: TwicPosition.right, mode: TwicMode.contain, refit: '10p'),
+    ];
     int indice = 0;
-    void changeRatio () {
+    void changeConfig () {
         setState(() {
-            indice = ( indice + 1 ) % ratios.length;
+            indice = ( indice + 1 ) % config.length;
         });
     }
 
@@ -29,15 +47,15 @@ class _RefitSampleState extends State<RefitSample> {
             body:  Container(
                 padding: const EdgeInsets.all(30),
                 child: TwicImg(
-                    src: 'refit/woman-in-canyon.jpeg',
-                    ratio: ratios[ indice ],
-                    refit: "15p",
-                    mode: TwicMode.cover,
+                    anchor: config[ indice ].anchor,
+                    src: 'components/refit/dog-looking-water.jpg',
+                    mode: config[ indice ].mode,
+                    refit: config[ indice ].refit,
                 ),
             ),
             floatingActionButton: FloatingActionButton.extended(
                 onPressed: () {
-                    changeRatio();
+                    changeConfig();
                 },
                 label: const Text( 'Change Refit' ),
                 backgroundColor: const Color.fromRGBO( 161, 52, 246, 1 ),
